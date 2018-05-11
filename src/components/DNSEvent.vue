e2527301cd876e497d4eabf3bf4ee6937ec1451d
王亚哲 <w4n9@sina.com>
2018-05-11 16:01:06 +0800

    new: 域名解析组件

---
+++ src/components/DNSEvent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    {{ title }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DNSEvent.vue',
+  data () {
+    return {
+      title: '这是域名解析页面'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
