18130bf42e203f590b95b5ee98395e3170e3e4a6
王亚哲 <w4n9@sina.com>
2018-05-11 15:59:02 +0800

    new: 首页组件

---
+++ src/components/Index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    {{ title }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index.vue',
+  data () {
+    return {
+      title: '这是首页'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
