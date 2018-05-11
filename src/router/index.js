d724237ead0be084b07867a34860707f220d5c22
王亚哲 <w4n9@sina.com>
2018-05-11 16:04:00 +0800

    new: 路由

---
+++ src/router/index.js
@@ -0,0 +1,60 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Layout from '@/components/Layout'
+import Index from '@/components/Index'
+import HTTPEvent from '@/components/HTTPEvent'
+import DNSEvent from '@/components/DNSEvent'
+import MalEvent from '@/components/MalEvent'
+import MalBehavior from '@/components/MalBehavior'
+import RuleStatic from '@/components/RuleStatic'
+import RuleCustom from '@/components/RuleCustom'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Layout',
+      component: Layout,
+      redirect: '/index',
+      children: [
+        {
+          path: '/index',
+          component: Index,
+          name: '首页'
+        },
+        {
+          path: '/http_event',
+          component: HTTPEvent,
+          name: '上网行为'
+        },
+        {
+          path: '/dns_event',
+          component: DNSEvent,
+          name: '域名解析'
+        },
+        {
+          path: '/mal_event',
+          component: MalEvent,
+          name: '恶意事件'
+        },
+        {
+          path: '/mal_behavior',
+          component: MalBehavior,
+          name: '恶意行为'
+        },
+        {
+          path: '/rule_static',
+          component: RuleStatic,
+          name: '静态规则'
+        },
+        {
+          path: '/rule_custom',
+          component: RuleCustom,
+          name: '自定义规则'
+        }
+      ]
+    }
+  ]
+})
