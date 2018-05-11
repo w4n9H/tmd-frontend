35b3c2e36818bf58352d46a05cc65abcb51b26a8
王亚哲 <w4n9@sina.com>
2018-05-11 16:01:50 +0800

    new: 恶意行为组件

---
+++ src/components/MalBehavior.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    {{ title }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MalBehavior.vue',
+  data () {
+    return {
+      title: '这是恶意行为页面'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
