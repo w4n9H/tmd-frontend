b3893374e20159ac2aa3dcf039ead2f495ec930d
王亚哲 <w4n9@sina.com>
2018-05-11 16:02:10 +0800

    new: 静态规则组件

---
+++ src/components/RuleStatic.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    {{ title }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RuleStatic.vue',
+  data () {
+    return {
+      title: '这是静态规则页面'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
