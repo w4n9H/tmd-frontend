b78cefdf474ec141b2ee508560b61ca1ea2aa785
王亚哲 <w4n9@sina.com>
2018-05-11 16:00:44 +0800

    new: 网络行为组件

---
+++ src/components/HTTPEvent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    {{ title }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HTTPEvent.vue',
+  data () {
+    return {
+      title: '这是上网行为页面'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
