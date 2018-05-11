e52ef0c7d23a0f8dc888318336213db8d2f8df01
王亚哲 <w4n9@sina.com>
2018-05-11 16:02:31 +0800

    new: 自定义规则组件

---
+++ src/components/RuleCustom.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    {{ title }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RuleCustom.vue',
+  data () {
+    return {
+      title: '这是自定义规则页面'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
