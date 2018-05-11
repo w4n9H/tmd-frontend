e32133fc1910d6efc0d3ecdb3a81680d75d2aa59
王亚哲 <w4n9@sina.com>
2018-05-11 16:01:29 +0800

    new: 恶意事件组件

---
+++ src/components/MalEvent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    {{ title }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MalEvent.vue',
+  data () {
+    return {
+      title: '这是恶意事件页面'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
