23c201a6751ce5d1dfcf70d93aa7e273d784316b
王亚哲 <w4n9@sina.com>
2018-05-15 18:20:04 +0800

    update: 修改整体布局

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,31 +1,33 @@
 <template>
-    <div>
+  <div>
+    <el-container>
+      <el-header style="text-align: center">
+        <div style="font-size: 28px; color: #000">{{ tmd_title }}</div>
+      </el-header>
+      <el-container>
+        <el-aside width="200px">
+          <el-menu :default-active="activeIndex" background-color="#000"
+                   text-color="#fff" active-text-color="#5eb2eb" router>
+            <el-menu-item index="index">首页</el-menu-item>
+            <el-menu-item index="http_event">上网行为</el-menu-item>
+            <el-menu-item index="dns_event">域名解析</el-menu-item>
+            <el-menu-item index="mal_event">恶意事件</el-menu-item>
+            <el-menu-item index="mal_behavior">恶意行为</el-menu-item>
+            <el-menu-item index="rule_static">静态规则</el-menu-item>
+            <el-menu-item index="rule_custom">自定义规则</el-menu-item>
+          </el-menu>
+        </el-aside>
         <el-container>
-            <el-header>
-                <div class="title-wrapper">
-                    <div class="title">
-                        <span>{{ tmd_title }}</span>
-                    </div>
-                </div>
-            </el-header>
-            <el-main>
-                <el-menu :default-active="activeIndex" class="el-menu" background-color="#476795"
-                text-color="#fff" active-text-color="#5eb2eb" mode="horizontal" router>
-                    <el-menu-item index="index">首页</el-menu-item>
-                    <el-menu-item index="http_event">上网行为</el-menu-item>
-                    <el-menu-item index="dns_event">域名解析</el-menu-item>
-                    <el-menu-item index="mal_event">恶意事件</el-menu-item>
-                    <el-menu-item index="mal_behavior">恶意行为</el-menu-item>
-                    <el-menu-item index="rule_static">静态规则</el-menu-item>
-                    <el-menu-item index="rule_custom">自定义规则</el-menu-item>
-                </el-menu>
-                <router-view></router-view>
-            </el-main>
-            <el-footer>
-                <p>此项目由个人维护并提供技术支持: w4n9ya@gmail.com</p>
-            </el-footer>
+          <el-main>
+            <router-view></router-view>
+          </el-main>
+          <el-footer>
+            <p style="text-align: center; font-size: 14px">此项目由个人维护并提供技术支持: w4n9ya@gmail.com</p>
+          </el-footer>
         </el-container>
-    </div>
+      </el-container>
+    </el-container>
+  </div>
 </template>
 
 <script>
