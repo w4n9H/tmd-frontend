3c2840ebe5481eecb3e3ea9a5c468ed641604228
王亚哲 <w4n9@sina.com>
2018-05-15 18:20:44 +0800

    update: 增加示例表格数据

--- src/components/HTTPEvent.vue
+++ src/components/HTTPEvent.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
-    {{ title }}
+    <el-breadcrumb separator="/">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item><a href="/">活动管理</a></el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-table :data="http_data" border style="width: 100%" height="250">
+      <el-table-column fixed prop="date" label="日期" width="180"></el-table-column>
+      <el-table-column prop="name" label="姓名" width="180"></el-table-column>
+      <el-table-column prop="address" label="地址"></el-table-column>
+    </el-table>
   </div>
 </template>
 
@@ -9,7 +17,23 @@ export default {
   name: 'HTTPEvent.vue',
   data () {
     return {
-      title: '这是上网行为页面'
+      http_data: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄'
+      }]
     }
   }
 }
