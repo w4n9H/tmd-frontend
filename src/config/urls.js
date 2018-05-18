8e135177e06633fb8b0e56e65fd7667197fadddb
王亚哲 <w4n9@sina.com>
2018-05-18 18:16:32 +0800

    update: urls请求接口列表

---
+++ src/config/urls.js
@@ -0,0 +1,6 @@
+export const URLs = {
+  normalTraffic: '/api/v1/normal_traffic',
+  malTraffic: '/api/v1/mal_traffic',
+  staticRule: '/api/v1/list_static_rule',
+  customRule: '/api/v1/list_custom_rule'
+}
