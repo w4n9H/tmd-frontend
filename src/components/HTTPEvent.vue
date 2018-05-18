53b2f3add8df87fba36f12b26e36187d01e8b290
王亚哲 <w4n9@sina.com>
2018-05-18 14:10:53 +0800

    update: 上网行为组件页面和样式

--- src/components/HTTPEvent.vue
+++ src/components/HTTPEvent.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="pageinfo">
     <div class="http_title">
       <el-breadcrumb>
         <el-breadcrumb-item>首页</el-breadcrumb-item>
@@ -7,7 +7,7 @@
       </el-breadcrumb>
     </div>
     <div class="http_search">
-      <el-form :inline="true" :model="formInline">
+      <el-form :inline="true" :model="formData">
         <el-form-item label="时间">
           <el-date-picker v-model="time_start_end" type="datetimerange"
                           range-separator="至"
@@ -16,25 +16,25 @@
             </el-date-picker>
         </el-form-item>
         <el-form-item label="源IP">
-          <el-input v-model="formInline.user" placeholder="源IP"></el-input>
+          <el-input v-model="formData.user" placeholder="源IP"></el-input>
         </el-form-item>
         <el-form-item label="源端口">
-          <el-input v-model="formInline.user" placeholder="源端口"></el-input>
+          <el-input v-model="formData.user" placeholder="源端口"></el-input>
         </el-form-item>
         <el-form-item label="目的IP">
-          <el-input v-model="formInline.user" placeholder="目的IP"></el-input>
+          <el-input v-model="formData.user" placeholder="目的IP"></el-input>
         </el-form-item>
         <el-form-item label="目的端口">
-          <el-input v-model="formInline.user" placeholder="目的端口"></el-input>
+          <el-input v-model="formData.user" placeholder="目的端口"></el-input>
         </el-form-item>
         <el-form-item label="请求方式">
-          <el-input v-model="formInline.user" placeholder="请求方式"></el-input>
+          <el-input v-model="formData.user" placeholder="请求方式"></el-input>
         </el-form-item>
         <el-form-item label="域名">
-          <el-input v-model="formInline.user" placeholder="域名"></el-input>
+          <el-input v-model="formData.user" placeholder="域名"></el-input>
         </el-form-item>
         <el-form-item label="设备">
-          <el-input v-model="formInline.user" placeholder="设备"></el-input>
+          <el-input v-model="formData.user" placeholder="设备"></el-input>
         </el-form-item>
         <el-form-item>
           <el-button type="primary" @click="onSubmit">查询</el-button>
@@ -42,20 +42,31 @@
       </el-form>
     </div>
     <div class="http_table">
-      <el-table :data="http_data" border style="width: 100%" height="250">
-        <el-table-column fixed prop="time" label="时间" width="180"></el-table-column>
-        <el-table-column prop="ttl" label="TTL" width="50"></el-table-column>
-        <el-table-column prop="len" label="LEN" width="50"></el-table-column>
-        <el-table-column prop="src_ip" label="源IP" width="150"></el-table-column>
-        <el-table-column prop="sport" label="源端口" width="80"></el-table-column>
-        <el-table-column prop="dst_ip" label="目的IP" width="150"></el-table-column>
-        <el-table-column prop="dport" label="目的端口" width="80"></el-table-column>
-        <el-table-column prop="method" label="请求方式" width="100"></el-table-column>
-        <el-table-column prop="host" label="请求HOST" width="100"></el-table-column>
-        <el-table-column prop="domain" label="域名" width="100"></el-table-column>
-        <el-table-column prop="uri" label="请求URI" width="100"></el-table-column>
-        <el-table-column prop="device" label="设备型号" width="100"></el-table-column>
+      <el-table :data="http_data" border size="mini">
+        <el-table-column fixed prop="time" label="时间" min-width="15%"></el-table-column>
+        <el-table-column prop="ttl" label="TTL" min-width="5%"></el-table-column>
+        <el-table-column prop="len" label="LEN" min-width="5%"></el-table-column>
+        <el-table-column prop="src_ip" label="源IP" min-width="10%"></el-table-column>
+        <el-table-column prop="sport" label="源端口" min-width="5%"></el-table-column>
+        <el-table-column prop="dst_ip" label="目的IP" min-width="10%"></el-table-column>
+        <el-table-column prop="dport" label="目的端口" min-width="5%"></el-table-column>
+        <el-table-column prop="method" label="请求方式" min-width="5%"></el-table-column>
+        <el-table-column prop="host" label="请求HOST" min-width="10%"></el-table-column>
+        <el-table-column prop="domain" label="域名" min-width="10%">
+          <template slot-scope="scope">
+            <el-tag type="success">{{ scope.row.domain }}</el-tag>
+          </template>
+        </el-table-column>
+        <el-table-column prop="uri" label="请求URI" min-width="10%"></el-table-column>
+        <el-table-column prop="device" label="设备型号" min-width="10%">
+          <template slot-scope="scope">
+            <el-tag type="success">{{ scope.row.device }}</el-tag>
+          </template>
+        </el-table-column>
       </el-table>
+      <div class="pagination">
+        <el-pagination background layout="prev, pager, next" :total="1000"></el-pagination>
+      </div>
     </div>
   </div>
 </template>
@@ -65,12 +76,51 @@ export default {
   name: 'HTTPEvent',
   data () {
     return {
-      formInline: {
+      formData: {
         user: '',
         region: ''
       },
       time_start_end: [],
       http_data: [{
+        time: '2016-05-02 10:23:59',
+        ttl: 64,
+        len: 456,
+        src_ip: '192.168.0.1',
+        sport: 65120,
+        dst_ip: '192.168.55.180',
+        dport: 80,
+        method: 'POST',
+        host: 'fanyi.baidu.com',
+        domain: 'baidu.com',
+        uri: '#en/zh/test',
+        device: 'iOS 10'
+      }, {
+        time: '2016-05-02 10:23:59',
+        ttl: 64,
+        len: 456,
+        src_ip: '192.168.0.1',
+        sport: 65120,
+        dst_ip: '192.168.55.180',
+        dport: 80,
+        method: 'POST',
+        host: 'fanyi.baidu.com',
+        domain: 'baidu.com',
+        uri: '#en/zh/test',
+        device: 'iOS 10'
+      }, {
+        time: '2016-05-02 10:23:59',
+        ttl: 64,
+        len: 456,
+        src_ip: '192.168.0.1',
+        sport: 65120,
+        dst_ip: '192.168.55.180',
+        dport: 80,
+        method: 'POST',
+        host: 'fanyi.baidu.com',
+        domain: 'baidu.com',
+        uri: '#en/zh/test',
+        device: 'iOS 10'
+      }, {
         time: '2016-05-02 10:23:59',
         ttl: 64,
         len: 456,
@@ -95,5 +145,25 @@ export default {
 </script>
 
 <style scoped>
-
+.pageinfo {
+  min-height: calc(100vh - 180px);
+}
+.http_title {
+  padding: 10px 10px;
+}
+.el-breadcrumb {
+  font-size: 16px;
+}
+.http_search {
+  padding: 10px 10px;
+}
+.http_table {
+  padding: 10px 10px;
+  width: 100%;
+  height: 100%;
+}
+.pagination {
+  text-align: right;
+  padding: 20px 10px;
+}
 </style>
