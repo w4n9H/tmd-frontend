2afbda06ff47f386c6d63fe9c032aa1a5628e6d2
王亚哲 <w4n9@sina.com>
2018-05-18 18:20:21 +0800

    update: 引入网络请求方法

--- src/main.js
+++ src/main.js
@@ -6,11 +6,15 @@ import 'element-ui/lib/theme-chalk/index.css'
 import './style/init.css'
 import App from './App'
 import router from './router'
+import {post, get} from './config/requests'
 
 /* 阻止vue 在启动时生成生产提示 */
 Vue.config.productionTip = true
 /* 引入Element */
 Vue.use(ElementUI)
+/* 引入请求方法 */
+Vue.prototype.$get = get
+Vue.prototype.$post = post
 
 /* eslint-disable no-new */
 new Vue({
