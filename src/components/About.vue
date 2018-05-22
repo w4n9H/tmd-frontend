1744df0303194d96f62e2ab994003f421b11298d
王亚哲 <w4n9@sina.com>
2018-05-22 18:01:34 +0800

    update: 新增关于TMD页面

---
+++ src/components/About.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="pageinfo">
+    <div class="about">
+      <el-breadcrumb>
+        <el-breadcrumb-item>首页</el-breadcrumb-item>
+        <el-breadcrumb-item>关于TMD</el-breadcrumb-item>
+      </el-breadcrumb>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About'
+}
+</script>
+
+<style scoped>
+.pageinfo {
+  min-height: calc(100vh - 180px);
+}
+.about {
+  padding: 10px 10px;
+}
+.el-breadcrumb {
+  font-size: 16px;
+}
+</style>
