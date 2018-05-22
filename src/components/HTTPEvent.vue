f0498b6c3db87f18c6144b43163bddfe8921b8e6
王亚哲 <w4n9@sina.com>
2018-05-22 18:04:35 +0800

    update: 基本完成上网行为页面

--- src/components/HTTPEvent.vue
+++ src/components/HTTPEvent.vue
@@ -9,32 +9,32 @@
     <div class="http_search">
       <el-form :inline="true" :model="formData">
         <el-form-item label="时间">
-          <el-date-picker v-model="timeStartEnd" type="datetimerange"
+          <el-date-picker v-model="formData.time" type="datetimerange"
                           range-separator="至"
                           start-placeholder="开始日期"
                           end-placeholder="结束日期">
             </el-date-picker>
         </el-form-item>
         <el-form-item label="源IP">
-          <el-input v-model="formData.user" placeholder="源IP"></el-input>
+          <el-input v-model="formData.src_ip" placeholder="源IP"></el-input>
         </el-form-item>
         <el-form-item label="源端口">
-          <el-input v-model="formData.user" placeholder="源端口"></el-input>
+          <el-input v-model="formData.sport" placeholder="源端口"></el-input>
         </el-form-item>
         <el-form-item label="目的IP">
-          <el-input v-model="formData.user" placeholder="目的IP"></el-input>
+          <el-input v-model="formData.dst_ip" placeholder="目的IP"></el-input>
         </el-form-item>
         <el-form-item label="目的端口">
-          <el-input v-model="formData.user" placeholder="目的端口"></el-input>
+          <el-input v-model="formData.dport" placeholder="目的端口"></el-input>
         </el-form-item>
         <el-form-item label="请求方式">
-          <el-input v-model="formData.user" placeholder="请求方式"></el-input>
+          <el-input v-model="formData.method" placeholder="请求方式"></el-input>
         </el-form-item>
         <el-form-item label="域名">
-          <el-input v-model="formData.user" placeholder="域名"></el-input>
+          <el-input v-model="formData.domain" placeholder="域名"></el-input>
         </el-form-item>
         <el-form-item label="设备">
-          <el-input v-model="formData.user" placeholder="设备"></el-input>
+          <el-input v-model="formData.device" placeholder="设备"></el-input>
         </el-form-item>
         <el-form-item>
           <el-button type="primary" @click="onSubmit">查询</el-button>
@@ -42,7 +42,7 @@
       </el-form>
     </div>
     <div class="http_table">
-      <el-table :data="httpData" v-loading="tableLoading" border size="mini">
+      <el-table :data="httpNewData" v-loading="tableLoading" border size="mini">
         <el-table-column fixed prop="ts" label="时间" min-width="15%"></el-table-column>
         <el-table-column prop="ttl" label="TTL" min-width="5%"></el-table-column>
         <el-table-column prop="len" label="LEN" min-width="5%"></el-table-column>
@@ -52,16 +52,20 @@
         <el-table-column prop="dport" label="目的端口" min-width="5%"></el-table-column>
         <el-table-column prop="method" label="请求方式" min-width="5%"></el-table-column>
         <el-table-column prop="host" label="请求HOST" min-width="10%" show-overflow-tooltip></el-table-column>
-        <el-table-column prop="domain" label="域名" min-width="10%">
-          <template slot-scope="scope">
-            <el-tag type="success">{{ scope.row.domain }}</el-tag>
-          </template>
-        </el-table-column>
+        <el-table-column prop="domain" label="域名" min-width="10%" show-overflow-tooltip></el-table-column>
         <el-table-column prop="uri" label="请求URI" min-width="10%" show-overflow-tooltip></el-table-column>
         <el-table-column prop="device" label="设备型号" min-width="10%" show-overflow-tooltip></el-table-column>
       </el-table>
-      <div class="pagination">
-        <el-pagination background layout="prev, pager, next" :total="1000"></el-pagination>
+      <div class="pagination" v-show="httpData.length >= 0">
+        <el-pagination background layout="sizes, prev, pager, next"
+                       @size-change="handleSizeChange"
+                       @current-change="handleCurrentChange"
+                       :current-page="pageSize"
+                       :total="httpData.length"
+                       :page-size="pageLimit"
+                       prev-text="上页"
+                       next-text="下页">
+        </el-pagination>
       </div>
     </div>
   </div>
@@ -75,31 +79,66 @@ export default {
   data () {
     return {
       formData: {
-        user: '',
-        region: ''
+        time: [],
+        src_ip: '',
+        sport: '',
+        dst_ip: '',
+        dport: '',
+        method: '',
+        domain: '',
+        device: ''
       },
-      timeStartEnd: [],
       httpData: [],
       tableLoading: false,
       pageSize: 1,
-      pageLimit: 20
+      pageLimit: 10,
+      pageDataTotal: 50
     }
   },
   created () { // 生命周期,页面加载是调用 fetchData 函数
     this.fetchData()
   },
+  computed: {
+    httpNewData () {
+      return this.httpData.slice(this.start, this.end)
+    },
+    start () {
+      return (this.pageSize - 1) * this.pageLimit
+    },
+    end () {
+      return this.pageSize * this.pageLimit
+    }
+  },
   methods: {
+    handleSizeChange (val) {
+      this.pageLimit = val
+      this.pageSize = 1
+    },
+    handleCurrentChange (val) {
+      this.pageSize = val
+    },
     onSubmit () {
-      console.log('submit!')
       const data = {
+        'page': this.pageSize,
+        'limit': this.pageDataTotal,
         'service': 'http'
+      }
+      for (let [k, v] of Object.entries(this.formData)) {
+        if (v.length > 0) {
+          if (Array.isArray(v) && v.length === 2) {
+            data['begin'] = v[0]
+            data['end'] = v[1]
+          } else {
+            data[k] = v
+          }
+        }
       }
       this.ReqData(URLs.normalTraffic, data)
     },
     fetchData () {
       const data = {
         'page': this.pageSize,
-        'limit': this.pageLimit,
+        'limit': this.pageDataTotal,
         'service': 'http'
       }
       this.ReqData(URLs.normalTraffic, data)
