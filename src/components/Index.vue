cf52b28c22c9d78bdb5cf4b8b577e335fc1a4e54
王亚哲 <w4n9@sina.com>
2018-05-29 07:45:32 +0800

    update: 去TMD首页,不用图表了,就写一些介绍吧

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,77 +1,28 @@
 <template>
-  <div>
-    <chart :options="polar"></chart>
+  <div class="pageinfo">
+    <div class="index_title">
+      <el-breadcrumb>
+        <el-breadcrumb-item>首页</el-breadcrumb-item>
+        <el-breadcrumb-item>总览</el-breadcrumb-item>
+      </el-breadcrumb>
+    </div>
+    <div class="index_body">
+    </div>
   </div>
 </template>
 
 <script>
-import ECharts from 'vue-echarts/components/ECharts'
-import 'echarts/lib/chart/bar'
-import 'echarts/lib/chart/line'
-import 'echarts/lib/chart/pie'
-import 'echarts/lib/chart/map'
-import 'echarts/lib/chart/radar'
-import 'echarts/lib/chart/scatter'
-import 'echarts/lib/chart/effectScatter'
-import 'echarts/lib/component/tooltip'
-import 'echarts/lib/component/polar'
-import 'echarts/lib/component/geo'
-import 'echarts/lib/component/legend'
-import 'echarts/lib/component/title'
-import 'echarts/lib/component/visualMap'
-import 'echarts/lib/component/dataset'
-
 export default {
-  components: {
-    chart: ECharts
-  },
-  data: function () {
-    let data = []
-    for (let i = 0; i <= 360; i++) {
-      let t = i / 180 * Math.PI
-      let r = Math.sin(2 * t) * Math.cos(2 * t)
-      data.push([r, i])
-    }
+  name: 'Index',
+  data () {
     return {
-      polar: {
-        title: {
-          text: '极坐标双数值轴'
-        },
-        legend: {
-          data: ['line']
-        },
-        polar: {
-          center: ['50%', '54%']
-        },
-        tooltip: {
-          trigger: 'axis',
-          axisPointer: {
-            type: 'cross'
-          }
-        },
-        angleAxis: {
-          type: 'value',
-          startAngle: 0
-        },
-        radiusAxis: {
-          min: 0
-        },
-        series: [
-          {
-            coordinateSystem: 'polar',
-            name: 'line',
-            type: 'line',
-            showSymbol: false,
-            data: data
-          }
-        ],
-        animationDuration: 2000
-      }
     }
   }
 }
 </script>
 
 <style scoped>
-
+.attack {
+  background-color: #555555;
+}
 </style>
