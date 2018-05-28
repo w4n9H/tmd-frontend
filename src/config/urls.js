771f1c366e5e4f43916f5ba58ccb7dc360a35bcb
王亚哲 <w4n9@sina.com>
2018-05-29 07:42:59 +0800

    update: 加入登录登出api

--- src/config/urls.js
+++ src/config/urls.js
@@ -1,4 +1,6 @@
 export const URLs = {
+  Login: '/api/v1/login',
+  Logout: '/api/v1/logout',
   normalTraffic: '/api/v1/normal_traffic',
   malTraffic: '/api/v1/mal_traffic',
   staticRule: '/api/v1/list_static_rule',
